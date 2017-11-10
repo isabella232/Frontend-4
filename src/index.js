4839df37a61c47a022af4f9b9ac848eec72c5b27
Thomas Poulet <1039134+Lord-Nazdar@users.noreply.github.com>
2017-11-10 15:37:54 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-import Search from './Search.vue'
+import Index from './Index.vue'
 import Travel from './Travel.vue'
 import Account from './Account.vue'
 import Guide from './Guide.vue'
@@ -11,11 +11,10 @@ Vue.use(VueRouter);
 Vue.use(VueResource)
 
 const routes = [
-  { path: '/', component: Search },
+  { path: '/', component: Index },
   { path: '/travel', component: Travel },
   { path: '/guide/:guideID/:view', name: 'guide', component: Guide, props: true },
   { path: '/account', name: 'account', component: Account}
-  // { path: '*', redirect: '/' }
 ]
 
 export var router = new VueRouter({
